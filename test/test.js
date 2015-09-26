09231a7efe0c42dcadeb7f2c2dea67e0f04bd7ea
Ivan Voischev <voischev.ivan@ya.ru>
2015-09-26 12:22:55 +0300

    Check doctype. Set tree options

--- test/test.js
+++ test/test.js
@@ -6,9 +6,9 @@ var expect = require('chai').expect;
 var HTML = '<html><head><title>Test</title></head><body><div class="button"><div class="button__text">Text</div></div></body></html>',
     DOCTYPE = '<!DOCTYPE html>';
 
-function test(input, output, doctype, done) {
+function test(input, output, options, done) {
     posthtml()
-        .use(dt(doctype))
+        .use(dt(options))
         .process(input)
         .then(function(result) {
             expect(output).to.eql(result.html);
@@ -23,7 +23,27 @@ describe('Simple test', function() {
         test(
             HTML,
             [DOCTYPE, HTML].join(''),
-            DOCTYPE,
+            { doctype: 'HTML 5' },
+            done
+        );
+    });
+
+    it('replace doctype', function(done) {
+        var doc = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">';
+        test(
+            [DOCTYPE, HTML].join(''),
+            [doc, HTML].join(''),
+            { doctype: 'HTML 4.01 Strict' },
+            done
+        );
+    });
+
+    it('check tree options', function(done) {
+        var doc = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">';
+        test(
+            '<html><head><link></head><body><img></body></html>',
+            [doc, '<html><head><link /></head><body><img /></body></html>'].join(''),
+            { doctype: 'HTML 4.01 Strict' },
             done
         );
     });
